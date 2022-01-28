cb54a46fa8fa701630a5cfceb18ebf475dadac38
tanveer <tanveerctg2014@gmail.com>
2022-01-28 22:26:18 +0600

    working on subtable

--- src/SubForm.js
+++ src/SubForm.js
@@ -1,36 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 //Material Ui
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import Box from "@mui/material/Box";
-import { Button } from "@mui/material";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
+import TextField from "@mui/material/TextField"
+import Autocomplete from "@mui/material/Autocomplete"
+import Box from "@mui/material/Box"
+import { Button } from "@mui/material"
+import IconButton from "@mui/material/IconButton"
+import CloseIcon from "@mui/icons-material/Close"
 //UUID
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid"
 
 export default function SubForm({ subformFields }) {
-  //return <div>SubForm:{JSON.stringify(subformFields)}</div>;
-  const [modulesForSubForm, setModulesForSubForm] = useState([]);
-  const [subforms, setSubforms] = useState([]);
+	//return <div>SubForm:{JSON.stringify(subformFields)}</div>;
+	const [modulesForSubForm, setModulesForSubForm] = useState([])
+	const [subforms, setSubforms] = useState([])
 
-  useEffect(() => {
-    setModulesForSubForm(subformFields);
-  }, []);
+	useEffect(() => {
+		setModulesForSubForm(subformFields)
+	}, [])
 
-  useEffect(() => {
-    subforms.forEach((subform) => {
-      if (subform.to && subform.from && subform.fieldMapping.length === 0) {
-        console.log("HIT API", subform.subform_id);
-      }
-    });
-  }, [subforms]);
+	useEffect(() => {
+		subforms.forEach((subform) => {
+			if (subform.to && subform.from && subform.fieldMapping.length === 0) {
+				console.log("HIT API", subform.subform_id)
+			}
+		})
+	}, [subforms])
 
-  return (
-    <>
-      {JSON.stringify({ modulesForSubForm })}
-      {JSON.stringify({ subforms })}
-      {/* <div>SubForm:{JSON.stringify(subformFields)}</div>
+	return (
+		<>
+			{JSON.stringify({ modulesForSubForm })}
+			{JSON.stringify({ subforms })}
+			{/* <div>SubForm:{JSON.stringify(subformFields)}</div>
       <Box sx={{ flex: 1 }}>
         <Autocomplete
           disablePortal
@@ -66,154 +66,160 @@ export default function SubForm({ subformFields }) {
         />
       </Box>
       <Box sx={{ width: "28px" }} /> */}
-      {subforms.map((subform, index) => (
-        <Box
-          sx={{
-            display: "flex",
-            maxWidth: "700px",
-            width: "100%",
-            gap: "15px",
-          }}
-          mt={2}
-          key={index}
-        >
-          {" "}
-          <Box sx={{ flex: 1 }}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              // options={subformFields.to}
-              options={modulesForSubForm.to.filter((subformModule) => {
-                // is field is already found in fieldMapping then deduct those fields from options
-                const isSelectedForMapping = subforms.find(
-                  (mappedField) => mappedField.to === subformModule.module
-                );
-                if (isSelectedForMapping) {
-                  return false;
-                } else {
-                  return true;
-                }
-              })}
-              value={
-                modulesForSubForm.to[
-                  modulesForSubForm.to.findIndex(
-                    (moduleSubform) => moduleSubform.module === subform.to
-                  )
-                ] || null
-              }
-              getOptionLabel={(option) => option.module}
-              onChange={(event, value) => {
-                console.log(value);
+			{subforms.map((subform, index) => (
+				<Box sx={{ width: "100%" }}>
+					<Box
+						sx={{
+							display: "flex",
 
-                const updatedSubforms = subforms.map((form) =>
-                  form.subform_id === subform.subform_id
-                    ? { ...form, to: value.module }
-                    : form
-                );
-                console.log({ updatedSubforms });
-                setSubforms(updatedSubforms);
-              }}
-              disableClearable={true}
-              // disabled={!!fromModuleName && !!toModuleName}
-              sx={{ width: "100%" }}
-              renderInput={(params) => (
-                <TextField {...params} label="Select Module" />
-              )}
-            />
-          </Box>
-          <Box sx={{ flex: 1 }}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={modulesForSubForm.from.filter((subformModule) => {
-                // is field is already found in fieldMapping then deduct those fields from options
-                const isSelectedForMapping = subforms.find(
-                  (mappedField) => mappedField.from === subformModule.module
-                );
-                if (isSelectedForMapping) {
-                  return false;
-                } else {
-                  return true;
-                }
-              })}
-              value={
-                modulesForSubForm.from[
-                  modulesForSubForm.from.findIndex(
-                    (moduleSubform) => moduleSubform.module === subform.from
-                  )
-                ] || null
-              }
-              getOptionLabel={(option) => option.module}
-              onChange={(event, value) => {
-                console.log(value);
+							gap: "15px",
+						}}
+						mt={2}
+						key={index}
+					>
+						{" "}
+						<Box sx={{ flex: 1 }}>
+							<Autocomplete
+								disablePortal
+								id='combo-box-demo'
+								// options={subformFields.to}
+								options={modulesForSubForm.to.filter((subformModule) => {
+									// is field is already found in fieldMapping then deduct those fields from options
+									const isSelectedForMapping = subforms.find(
+										(mappedField) =>
+											mappedField.toModuleName === subformModule.module
+									)
+									if (isSelectedForMapping) {
+										return false
+									} else {
+										return true
+									}
+								})}
+								value={
+									modulesForSubForm.to[
+										modulesForSubForm.to.findIndex(
+											(moduleSubform) =>
+												moduleSubform.module === subform.toModuleName
+										)
+									] || null
+								}
+								getOptionLabel={(option) => option.module}
+								onChange={(event, value) => {
+									console.log(value)
 
-                const updatedSubforms = subforms.map((form) =>
-                  form.subform_id === subform.subform_id
-                    ? { ...form, from: value.module }
-                    : form
-                );
-                console.log({ updatedSubforms });
-                setSubforms(updatedSubforms);
-              }}
-              disableClearable={true}
-              // disabled={!!fromModuleName && !!toModuleName}
-              sx={{ width: "100%" }}
-              renderInput={(params) => (
-                <TextField {...params} label="Select Module" />
-              )}
-            />
-          </Box>
-          <Box>
-            <IconButton
-              aria-label="delete"
-              size="small"
-              onClick={() => {
-                console.log(subform.subform_id);
+									const updatedSubforms = subforms.map((form) =>
+										form.subform_id === subform.subform_id
+											? { ...form, toModuleName: value.module }
+											: form
+									)
+									console.log({ updatedSubforms })
+									setSubforms(updatedSubforms)
+								}}
+								disableClearable={true}
+								// disabled={!!fromModuleName && !!toModuleName}
+								sx={{ width: "100%" }}
+								renderInput={(params) => (
+									<TextField {...params} label='Select Module' />
+								)}
+							/>
+						</Box>
+						<Box sx={{ flex: 1 }}>
+							<Autocomplete
+								disablePortal
+								id='combo-box-demo'
+								options={modulesForSubForm.from.filter((subformModule) => {
+									// is field is already found in fieldMapping then deduct those fields from options
+									const isSelectedForMapping = subforms.find(
+										(mappedField) =>
+											mappedField.fromModuleName === subformModule.module
+									)
+									if (isSelectedForMapping) {
+										return false
+									} else {
+										return true
+									}
+								})}
+								value={
+									modulesForSubForm.from[
+										modulesForSubForm.from.findIndex(
+											(moduleSubform) =>
+												moduleSubform.module === subform.fromModuleName
+										)
+									] || null
+								}
+								getOptionLabel={(option) => option.module}
+								onChange={(event, value) => {
+									console.log(value)
 
-                const findSubform = subforms.find(
-                  (form) => form.subform_id === subform.subform_id
-                );
-                // console.log({ findSubform });
+									const updatedSubforms = subforms.map((form) =>
+										form.subform_id === subform.subform_id
+											? { ...form, fromModuleName: value.module }
+											: form
+									)
+									console.log({ updatedSubforms })
+									setSubforms(updatedSubforms)
+								}}
+								disableClearable={true}
+								// disabled={!!fromModuleName && !!toModuleName}
+								sx={{ width: "100%" }}
+								renderInput={(params) => (
+									<TextField {...params} label='Select Module' />
+								)}
+							/>
+						</Box>
+						<Box>
+							<IconButton
+								aria-label='delete'
+								size='small'
+								onClick={() => {
+									console.log(subform.subform_id)
 
-                const filterSubforms = subforms.filter(
-                  (module) => module.subform_id !== subform.subform_id
-                );
-                setSubforms(filterSubforms);
-                console.log({ filterSubforms });
-                // setModulesForSubForm(prev=>prev.filter(module=>module.subform_id!==subform.subform_id))
-              }}
-            >
-              <CloseIcon fontSize="inherit" />
-            </IconButton>
-          </Box>
-        </Box>
-      ))}
+									const findSubform = subforms.find(
+										(form) => form.subform_id === subform.subform_id
+									)
+									// console.log({ findSubform });
 
-      <Box
-        sx={{
-          display: "flex",
-          maxWidth: "700px",
-          width: "100%",
-          gap: "15px",
-          justifyContent: "center",
-        }}
-        mt={2}
-      >
-        <Button
-          onClick={() =>
-            setSubforms((prev) => [
-              ...prev,
-              { subform_id: uuidv4(), fieldMapping: [] },
-            ])
-          }
-          variant="outlined"
-          size="small"
-        >
-          Add Subform
-        </Button>
-      </Box>
-    </>
-  );
+									const filterSubforms = subforms.filter(
+										(module) => module.subform_id !== subform.subform_id
+									)
+									setSubforms(filterSubforms)
+									console.log({ filterSubforms })
+									// setModulesForSubForm(prev=>prev.filter(module=>module.subform_id!==subform.subform_id))
+								}}
+							>
+								<CloseIcon fontSize='inherit' />
+							</IconButton>
+						</Box>
+					</Box>
+					<Button>Add Field</Button>
+				</Box>
+			))}
+
+			<Box
+				sx={{
+					display: "flex",
+					maxWidth: "700px",
+					width: "100%",
+					gap: "15px",
+					justifyContent: "center",
+				}}
+				mt={2}
+			>
+				<Button
+					onClick={() =>
+						setSubforms((prev) => [
+							...prev,
+							{ subform_id: uuidv4(), fieldMapping: [] },
+						])
+					}
+					variant='outlined'
+					size='small'
+				>
+					Add Subform
+				</Button>
+			</Box>
+		</>
+	)
 }
 
 //[{to:'name',from:'name',id:0923498i543298,fieldMapping:[]}]
